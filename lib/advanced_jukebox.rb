74033bd0da56ecf83ad61d119cc259ed8d8183fc
Robert Weber <rob.weber87@gmail.com>
2017-03-08 02:36:12 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,26 +2,39 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+my_songs = {
+"Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
+"LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
+"Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
+"Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
+"Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+}
 
 def help
   #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+  my_songs_array = my_songs.keys
+  idx = 1
+
+  my_songs.each do |song_name, location|
+    puts "#{idx}. #{song_name}"
+    idx += 1
+  end
+end
+
 end
 
 
@@ -33,13 +46,35 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  puts "Goodbye."
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  help
+
+  input = ""
+  while input
+    puts "Please enter a command:"
+    input = gets.downcase.strip
+    case input
+    when 'list'
+      list(my_songs)
+    when 'play'
+      list(my_songs)
+      play(my_songs)
+    when 'help'
+      help
+    when 'exit'
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,63 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+
+def list(songs)
+  idx = 1
+
+  songs.each do |song_name|
+    puts "#{idx}. #{song_name}"
+    idx += 1
+  end
+end
+
+def play(songs)
+
+  puts "Please enter a song name or number:"
+  user_response = gets.chomp
+
+  if (1..songs.length).to_a.include?(user_response.to_i)
+    puts "Playing #{songs[user_response.to_i - 1]}"
+  elsif songs.include?(user_response)
+    puts "Playing #{user_response}"
+  else
+    puts "Invalid input, please try again"
+  end
+
+end
+
+def exit_jukebox
+
+  puts "Goodbye."
+end
+
+def run(songs)
+
+  user_response = ""
+  while user_response
+    puts "Please enter a command:"
+    user_response = gets.chomp
+    case user_response
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
+
+end
